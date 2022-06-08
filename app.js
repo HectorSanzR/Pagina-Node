2257e510faa69e5def7d04f38c951cc711f3a976
Hector Sanchez <hectorsanzr94@gmail.com>
2022-06-07 19:27:14 -0500

    webServer

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express')
+const hbs = require('hbs');
+
+const app = express()
+const port= process.env.PORT
+
+
+
+
+
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+//servir contenido estatico 
+app.use(express.static('public'))
+
+app.get('/', (req, res)=> {
+    res.render('home',{
+        nombre: 'Hector Sanchez',
+        titulo: 'Curso de Node'
+    });
+    // res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/generic', (req, res)=> {
+  res.render('generic',{
+    nombre: 'Hector Sanchez',
+    titulo: 'Curso de Node'
+});
+  })
+  app.get('/elements', (req, res)=> {
+    res.render('elements',{
+      nombre: 'Hector Sanchez',
+      titulo: 'Curso de Node'
+  });
+  })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
